a6ef8f0fd66d7d6bc91ad2f1d02e110f8b507d9f
Marjai Mirko <marjaimirko@gmail.com>
2022-09-30 16:17:52 +0200

    first

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -9,12 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UserMaintenance
+
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; 
+            label2.Text = Resource1.FirstName; 
+            button1.Text = Resource1.Add; 
         }
     }
 }
