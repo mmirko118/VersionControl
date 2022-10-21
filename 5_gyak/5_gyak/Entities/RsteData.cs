f3d0d3ac49baf6c2ff06b3ad400790b59c13e3a1
Mirkó Marjai <mirko.marjai@stud.uni-corvinus.hu>
2022-10-21 10:57:16 +0200

    class

---
+++ 5_gyak/5_gyak/Entities/RsteData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_gyak.Entities
+{
+    class RsteData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public int Value { get; set; }
+
+    }
+}
--- 5_gyak/5_gyak/Form1.cs
+++ 5_gyak/5_gyak/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _5_gyak.Entities;
+using _5_gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +13,24 @@ using System.Windows.Forms;
 namespace _5_gyak
 {
     public partial class Form1 : Form
+
     {
+
+        BindingList<RsteData> Rate = new BindingList<RsteData>();
         public Form1()
+
         {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
             InitializeComponent();
         }
     }
