7a3a4a169c8a6da035532b22053f1a3167efec66
Mirkó Marjai <mirko.marjai@stud.uni-corvinus.hu>
2022-12-02 10:19:09 +0100

    classes

---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    class BirthProbability
+    {
+        public int kor { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double IsBirth { get; set; }
+    }
+}
